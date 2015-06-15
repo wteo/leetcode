cfd7c7e11d00ea3c1be0a50e82dd49516c4dbc0c
wteo <wteo@users.noreply.github.com>
2015-06-15 11:35:19 +0800

    Update House Robber.cpp

--- House Robber.cpp
+++ House Robber.cpp
@@ -13,9 +13,11 @@ determine the maximum amount of money you can rob tonight without alerting the p
 
 /*
 Algorithm:
-Loop for all elements in arr[] and maintain two sums incl and excl where incl = Max sum including the previous element and excl = Max sum excluding the previous element.
+Loop for all elements in arr[] and maintain two sums incl and excl where incl = Max sum including 
+the previous element and excl = Max sum excluding the previous element.
 
-Max sum excluding the current element will be max(incl, excl) and max sum including the current element will be excl + current element (Note that only excl is considered because elements cannot be adjacent).
+Max sum excluding the current element will be max(incl, excl) and max sum including the current element 
+will be excl + current element (Note that only excl is considered because elements cannot be adjacent).
 
 At the end of the loop return max of incl and excl.
 */
